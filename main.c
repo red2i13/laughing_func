5bd6ac968e305812b3f77af6780fb24f23e49201
red2i13 <129304172+red2i13@users.noreply.github.com>
2024-02-26 05:22:18 +0000

    hello

--- main.c
+++ main.c
@@ -1,130 +1,118 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-#define ROW 9
-#define COL 10
-typedef struct Node {
-    int x, y; 
+#define row 5
+#define col 5
 
-    struct Node* parent;
-    struct Node* next;
-    int content;
-    int visited;
- 
-} Node;
-Node* createNode(int x, int y, Node* parent) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    //printf("create NOde %i %i\n", x, y);
-    newNode->x = x;
-    newNode->y = y;
-    newNode->parent = parent;
-    return newNode;
-}
-void print_grid(int grid[ROW][COL])
+typedef struct node{
+    struct node *next;
+    struct node *parent;
+    int x;
+    int y;
+}node;
+
+void print_vis(int arr[row][col])
 {
-    for(int i = 0; i < ROW; i++)
+    for(int i = 0; row >i; i++)
     {
-        for(int j = 0; j < COL; j++)
+        for (int j = 0; j < col; j++)
         {
-            printf("%i ", grid[i][j]);
+            printf("%i ", arr[i][j]);
         }
         printf("\n");
     }
 }
-int insert(Node **list, int x, int y)
+void print_list(node *lst)
 {
-    Node *newnode = createNode(x, y, NULL);
-
-    newnode->parent = *list;
-    *list = newnode;
-    return(EXIT_SUCCESS);
-}
-int search(Node *queue, Node *visited)
-{
-    //Node *tmp = queue;
-    Node *tmp1 = visited;
-
-    while (tmp1)
+    node  *tmp;
+    tmp = lst;
+    int i = 1;
+    
+    if(!lst)
+        printf("THE LIST IS EMPTY\n");
+    while (tmp)
     {
-        if(tmp1->x == queue->x && tmp1->y == queue->y)
-            return(1);
-        tmp1 = tmp1->parent; 
+        printf("Node N:%i tmp->x = %i , tmp->y = %i\n", i++, tmp->x, tmp->y);
+        tmp = tmp->next;
     }
-    return(0);
-}
-int neighbors_search(Node **queue, Node **visited, int grid[ROW][COL],int x, int y)
-{
-    int i;
-    Node *current = (*queue);
-    printf("neihbors x = %i y = %i\n", x, y);
-    if(y + 1 < COL && grid[y+1][x] != 1)
-    {
-        insert(queue, x, y+1);
-        //printf("inserted %i %i\n", x, y+1);
-    }
-    else if(y - 1 > 0 && grid[y-1][x] != 1)
-        insert(queue, x, y-1);
-    else if(x+1 < COL && grid[y][x+1] != 1)
-        insert(queue, x + 1, y);
-    else if(x - 1 > 0&& grid[y][x-1] != 1)
-        insert(queue, x - 1, y);
-    return(EXIT_SUCCESS);
-}
-int pop(Node **queue, int x, int y)
-{
-    Node *current = *queue;
-    while (current)
-    {
-        if(current->x == x && current->y == y)
-        {
-            current->parent = NULL;
-            free(current);
-            return(1);
-        }
-        current = current->parent;
-    }
-    return(0);
     
 }
-int bfs(int grid[ROW][COL], int x, int y)
+int enqueue(node **queue, int x, int y)
 {
-    Node **queue  = malloc(sizeof(Node *));
-    Node **visited = malloc(sizeof(Node *));
+    node *new_node = malloc(sizeof(node));
+    if(!new_node)
+        return(1);
+    new_node->x = x;
+    new_node->y = y;
 
-    (*queue) = createNode(x, y, NULL);
-    Node *current = (*queue);
-    while (current)
+    if ((*queue) == NULL)
     {
-        //printf("SEG FAULTT  TTTTTT\n");
-        if (!search(current, *visited))
-        {
-            printf("grid[%i][%i] = \n", current->y, current->x);
-            printf("%i\n", grid[current->y][current->x]);
-            grid[current->y][current->x] = 7;
-            neighbors_search(queue, visited,grid, current->x, current->y);
-            insert(visited, current->x, current->y);
-            pop(queue, current->x, current->y);
-        }
-        //printf("after grid[%i][%i] = %i\n", current->x, current->y, grid[current->y][current->x]);
-        current = *queue;
+        printf("DEBUUUUUUUUG\n");
+        (*queue) = new_node;
+        (*queue)->parent = (*queue)->next = 0;
+
+    }
+    else if ((*queue)->parent == NULL)
+    {
+        new_node->next = 0;
+        new_node->parent = (*queue);
+        (*queue)->next = new_node;
+    }
+    else
+    {
+        new_node->next = (*queue)->next;
+        new_node->parent = (*queue);
+        (*queue)->next = new_node;
     }
     return(EXIT_SUCCESS);
+}
+int dequeue(node **queue, node *cur)
+{
+    node *tmp = (*queue);
+    while (tmp)
+    {
+        if (tmp == (*queue))
+        {
+            tmp->next->parent = NULL;
+            (*queue) = (*queue)->next;
+            free(tmp);
+            return(0);
+        }
+        tmp = tmp->next
+    }
+    return(1);
+    
+}
+int bfs(int st_x, int st_y)
+{
+    int arr[row][col]; 
+    memset(arr, 0, sizeof(arr));
+    print_vis(arr);
+    node *queue;
+    queue = NULL;
+    print_list(queue);
+    enqueue(&queue, st_x, st_y);
+    print_list(queue);
+    printf("---------------------\n");
+    enqueue(&queue, 5, 6);
+    print_list(queue);
+
+    exit(5);
+    return(EXIT_SUCCESS);
+
 }
 
 int main()
 {
-    int grid[ROW][COL]
-        = { { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-            { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
-            { 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
-            { 1, 0, 1, 0, 1, 0, 0, 0, 0, 1 },
-            { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
-            { 1, 0, 1, 1, 1, 1, 0, 1, 0, 1 },
-            { 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
-            { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
-            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
-    print_grid(grid);
-    bfs(grid, 3, 1);
-    print_grid(grid);
-    return(EXIT_SUCCESS);
+    bfs(3, 5);
+     int a [7][7] = {
+        {0,1,1,0,0,0,0},
+        {1,0,0,1,0,0,0},
+        {1,0,0,1,1,0,0},
+        {0,1,1,0,0,0,0},
+        {0,0,1,0,0,1,1},
+        {0,0,0,0,1,0,1}, 
+        {0,0,0,0,1,1,0} 
+    };
 }
---
+++ test.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure to represent a node in the graph
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Structure to represent a queue for BFS
+typedef struct Queue {
+    Node* front;
+    Node* rear;
+} Queue;
+
+// Function to create a new node
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create an empty queue
+Queue* createQueue() {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->front = queue->rear = NULL;
+    return queue;
+}
+
+// Function to check if the queue is empty
+int isEmpty(Queue* queue) {
+    return (queue->front == NULL);
+}
+
+// Function to enqueue a node into the queue
+void enqueue(Queue* queue, int data) {
+    Node* newNode = createNode(data);
+    if (isEmpty(queue)) {
+        queue->front = queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
+}
+
+// Function to dequeue a node from the queue
+int dequeue(Queue* queue) {
+    if (isEmpty(queue)) {
+        printf("Queue is empty!\n");
+        return -1;
+    }
+    Node* temp = queue->front;
+    int data = temp->data;
+    queue->front = queue->front->next;
+    free(temp);
+    return data;
+}
+
+// Breadth-First Search function
+void BFS(int** graph, int vertices, int startVertex) {
+    int visited[vertices];
+    for (int i = 0; i < vertices; i++) {
+        visited[i] = 0;
+    }
+    Queue* queue = createQueue();
+    visited[startVertex] = 1;
+    enqueue(queue, startVertex);
+    while (!isEmpty(queue)) {
+        int currentVertex = dequeue(queue);
+        printf("%d ", currentVertex);
+        for (int i = 0; i < vertices; i++) {
+            if (graph[currentVertex][i] == 1 && !visited[i]) {
+                visited[i] = 1;
+                enqueue(queue, i);
+            }
+        }
+    }
+    printf("\n");
+}
+
+// Sample usage
+int main() {
+    int vertices = 6;
+    int** graph = (int**)malloc(vertices * sizeof(int*));
+    for (int i = 0; i < vertices; i++) {
+        graph[i] = (int*)malloc(vertices * sizeof(int));
+    }
+    // Sample graph initialization
+    int sampleGraph[6][6] = {
+        {0, 1, 1, 0, 0, 0},
+        {1, 0, 0, 1, 1, 0},
+        {1, 0, 0, 0, 1, 0},
+        {0, 1, 0, 0, 1, 1},
+        {0, 1, 1, 1, 0, 1},
+        {0, 0, 0, 1, 1, 0}
+    };
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+            graph[i][j] = sampleGraph[i][j];
+        }
+    }
+    printf("BFS Traversal: ");
+    BFS(graph, vertices, 0); // Starting BFS from vertex 0
+    // Free allocated memory
+    for (int i = 0; i < vertices; i++) {
+        free(graph[i]);
+    }
+    free(graph);
+    return 0;
+}
